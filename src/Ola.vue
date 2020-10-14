938f9312a15a197f512da4fbe4ba0626dbba2fa3
Lupa Stance <genzokun@gmail.com>
2020-10-14 16:16:06 +0200

    gfdg
    dfgdfg

---
+++ src/Ola.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hola desde Ola !</h1>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    body{
+        color: red;
+    }
+</style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
     import Vue from 'vue'
     import UserPop from './UserPop.vue'
+    import Ola from './Ola.vue'
 
     export default {
         install(Vue, options) {
             Vue.component('user-pop', UserPop)
+            Vue.component('ola', Ola)
         }
     }
